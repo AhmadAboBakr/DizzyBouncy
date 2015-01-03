50c441bf90158bfb1d24436016d02e64e163b477
AhmadAboBakr <ahmadabobakr@gmail.com>
2015-01-03 04:16:12 +0200

    refactoring and fixing poinky look
    poinky is now updated and drawn from it's own class
    removed one of the fps counters

--- js/Poinky.js
+++ js/Poinky.js
@@ -4,21 +4,35 @@
     this.effect = effect
     this.baseBallStrength = 1.2;
     this.midBallStrength = .8;
-    var baseBallPosition, midBallPosition;
+    this.topBallStrength = 0.4;
+    var baseBallPosition, midBallPosition,topBallPosition;
     this.update = function () {
-        baseBallPosition = new three.Vector3(midBall.position.x, midBall.position.y, midBall.position.z);
+        baseBallPosition = new three.Vector3(baseBall.position.x, baseBall.position.y, baseBall.position.z);
         this.effect.worldToLocal(baseBallPosition);
         baseBallPosition.x = (baseBallPosition.x + 1) / 2;
         baseBallPosition.y = (baseBallPosition.y + 1) / 2;
         baseBallPosition.z = (baseBallPosition.z + 1) / 2;
 
-        midBallPosition = new three.Vector3(midBall.position.x, midBall.position.y, midBall.position.z);
+        midBallPosition = new three.Vector3(midBall.position.x, midBall.position.y-5, midBall.position.z);
         this.effect.worldToLocal(midBallPosition);
         midBallPosition.x = (midBallPosition.x + 1) / 2;
         midBallPosition.y = (midBallPosition.y + 1) / 2;
         midBallPosition.z = (midBallPosition.z + 1) / 2;
+
+        topBallPosition = new three.Vector3(midBall.position.x, midBall.position.y, midBall.position.z);
+        this.effect.worldToLocal(topBallPosition);
+        topBallPosition.x = (topBallPosition.x + 1) / 2;
+        topBallPosition.y = (topBallPosition.y + 1) / 2;
+        topBallPosition.z = (topBallPosition.z + 1) / 2;
+
     }
     this.draw = function () {
+        this.effect.addBall(
+            topBallPosition.x,
+            topBallPosition.y,
+            topBallPosition.z,
+            this.topBallStrength, 12
+            );
         this.effect.addBall(
             midBallPosition.x,
             midBallPosition.y,
