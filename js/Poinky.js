70d4da7e42cf242bd2d71ef770e228d4842386ee
AhmadAboBakr <ahmadabobakr@gmail.com>
2015-01-03 08:44:20 +0200

    update is now called when poinky is intialized

--- js/Poinky.js
+++ js/Poinky.js
@@ -5,7 +5,8 @@
     this.baseBallStrength = 1.2;
     this.midBallStrength = .8;
     this.topBallStrength = 0.4;
-    var baseBallPosition, midBallPosition,topBallPosition;
+    var baseBallPosition, midBallPosition, topBallPosition;
+    this.update();
     this.update = function () {
         baseBallPosition = new three.Vector3(baseBall.position.x, baseBall.position.y, baseBall.position.z);
         this.effect.worldToLocal(baseBallPosition);
